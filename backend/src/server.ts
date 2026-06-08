ac0998584be2d543903127704d927e4332bb6c2a
backendcodigix <backendcodigix@gmail.com>
2026-06-08 12:53:39 +0530

    push

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,10 @@ import pool from './db';
 
 const app: Express = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.ORIGIN,
+  credentials: true,
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
